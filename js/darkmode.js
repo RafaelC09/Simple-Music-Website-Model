15005f51b4e9911b27058f22afea42727417c342
RafaelC09 <rafael.ap.carvalho0909@gmail.com>
2025-07-03 16:39:50 +0100

    Update 6

---
+++ js/darkmode.js
@@ -0,0 +1,20 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+document.getElementById("toggle-theme").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        this.textContent = "Light Mode";
+        btn1.style.backgroundColor = '#111111';
+        btn1.style.color = '#ffffff';
+        btn2.style.backgroundColor = '#111111';
+        btn2.style.color = '#ffffff';
+    } else {
+        this.textContent = "Dark Mode";
+        btn1.style.color = '#111111';
+        btn1.style.backgroundColor = '#ffffff';
+        btn2.style.color = '#111111';
+        btn2.style.backgroundColor = '#ffffff';
+    }
+});
